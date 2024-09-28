a186f8f5d51d2a3d58a613796e992510180afb28
Victor <victorh.csilva20@gmail.com>
2024-09-28 15:22:19 -0300

    Apenas inserção no banco

--- projeto-integrador/Django/SAGe/PI_SAGe/views.py
+++ projeto-integrador/Django/SAGe/PI_SAGe/views.py
@@ -1,35 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Cadastro
+from .models import Pacientes, Medicos
 
 def home(request):
     return render(request, 'site_SAGe/home.html')
 
-def portal_do_paciente(request):
-    return render(request, 'site_SAGe/gerenciar.html')
+def gerenciar(request):
+    if request.method == 'POST':  # Verifica se o formulário foi enviado
+        novo_usuario = Pacientes()
+        novo_usuario.nome = request.POST.get('nome')
+        novo_usuario.idade = request.POST.get('idade')
+        novo_usuario.cpf = request.POST.get('cpf')
+        novo_usuario.data_consulta = request.POST.get('data_consulta')
+        novo_usuario.telefone = request.POST.get('telefone')
+        novo_usuario.save()
+
+        # Redireciona para a página de gerenciar
+        return redirect('gerenciar')  # Adiciona o redirect aqui
+
+    pacientes = Pacientes.objects.all()
+    medicos = Medicos.objects.all()
+    
+    context = {
+        "pacientes":pacientes,
+        "medicos":medicos,
+    }
+    
+    return render(request, 'site_SAGe/gerenciar.html', context ) 
 
 def sobre(request):
     return render(request, 'site_SAGe/sobre.html')
 
+#formulario
 def cadastro(request):
     return render(request, 'site_SAGe/cadastro.html')
 
 def login(request):
     return render(request, 'site_SAGe/login.html')
 
-def cadastrados(request):
-    novo_usuario = Cadastro()
-    novo_usuario.nome = request.POST.get('nome')
-    novo_usuario.idade = request.POST.get('idade')
-    novo_usuario.senha = request.POST.get('senha')
-    novo_usuario.cpf = request.POST.get('cpf')
-    novo_usuario.data = request.POST.get('data')
-    novo_usuario.telefone = request.POST.get('telefone')
-    novo_usuario.save()
-    
-    #Exibir dados
-    cadastros = {
-        'cadastrados':Cadastro.objects.all()
-    }
-    #Retornar dados
-    return render(request,'PI_SAGe/cadastro.html', cadastros)
--- projeto-integrador/Django/SAGe/SAGe/urls.py
+++ projeto-integrador/Django/SAGe/SAGe/urls.py
@@ -3,7 +3,7 @@ from PI_SAGe import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # URL base para a página inicial
-    path('gerenciar/', views.portal_do_paciente, name='gerenciar'),
+    path('gerenciar/', views.gerenciar, name='gerenciar'),
     path('sobre/', views.sobre, name='sobre'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.login, name='login'),
